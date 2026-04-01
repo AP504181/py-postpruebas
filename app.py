89ac1d15cdeddee80e2bfb2a6ecab0f7e1503232
Alfredo Pérez Mendoza <alfredo.perez1@profuturo.com.mx>
2026-04-01 14:38:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 URL = "https://www.proinversion.mx:2180/mb/fileNet/cargaArchivo"
 #URL = "http://172.16.48.215:7080/mb/fileNet/cargaArchivo"
+#URL = "https://172.16.80.227:2180/mb/fileNet/cargaArchivo"
 
 # Credenciales (ya vienen en base64 normalmente, pero aquí lo dejamos directo)
 AUTH = "Basic cndzcHJheGlzcDpQcjR4MXMjdTVS"
